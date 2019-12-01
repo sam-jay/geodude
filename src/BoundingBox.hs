2e30d83ee5b2770052a0e37f6176cd44487f1bef
Sam Jayasinghe <sj2564@columbia.edu>
2019-12-01 11:25:23 -0500

    reformat BoundingBox

--- src/BoundingBox.hs
+++ src/BoundingBox.hs
@@ -2,14 +2,18 @@ module BoundingBox where
 
 import Data.List (intersperse)
 
-data BoundingBox = BoundingBox {x1 :: !Double, y1 :: !Double, x2 :: !Double, y2 :: !Double}
-  deriving (Eq)
+data BoundingBox = BoundingBox { x1 :: !Double
+                               , y1 :: !Double
+                               , x2 :: !Double
+                               , y2 :: !Double } deriving (Eq)
 
 type Point = (Double, Double)
 
 --get the least enlargement boundingbox that contains bb1 and bb2
 enlarge :: BoundingBox -> BoundingBox -> BoundingBox
-enlarge (BoundingBox x1 y1 x2 y2) (BoundingBox x1' y1' x2' y2') = BoundingBox (min x1 x1') (min y1 y1') (max x2 x2') (max y2 y2')
+enlarge b1 b2 = BoundingBox (min x1 x1') (min y1 y1') (max x2 x2') (max y2 y2')
+    where BoundingBox x1 y1 x2 y2 = b1
+          BoundingBox x1' y1' x2' y2' = b2
 
 -- compute the area of a boundingbox
 area :: BoundingBox -> Double
@@ -17,15 +21,20 @@ area (BoundingBox x1 y1 x2 y2) = (x2 - x1) * (y2 - y1)
 
 --check whether the first boundingbox contains the second boundingbox
 containsRect :: BoundingBox -> BoundingBox -> Bool
-containsRect (BoundingBox x1 y1 x2 y2) (BoundingBox x1' y1' x2' y2') = x1 <= x1' && y1 <= y1' && x2 >= x2' && y2 >= y2'
+containsRect b1 b2 = x1 <= x1' && y1 <= y1' && x2 >= x2' && y2 >= y2'
+    where BoundingBox x1 y1 x2 y2 = b1
+          BoundingBox x1' y1' x2' y2' = b2
 
 --check whether a boundingbox contains a point
 containsPoint :: BoundingBox -> Point -> Bool
-containsPoint (BoundingBox x1 y1 x2 y2) (px, py) = px >= x1 && px <= x2 && py >= y1 && py <= y2
+containsPoint bb (px, py) = px >= x1 && px <= x2 && py >= y1 && py <= y2
+    where BoundingBox x1 y1 x2 y2 = bb
 
 --check whether two boundingbox intersect
 intersect :: BoundingBox -> BoundingBox -> Bool
-intersect (BoundingBox x1 y1 x2 y2) (BoundingBox x1' y1' x2' y2') = not (x1 >= x2' || x2 <= x1' || y2 <= y1' || y2' <= y1)
+intersect b1 b2 = not (x1 >= x2' || x2 <= x1' || y2 <= y1' || y2' <= y1)
+    where BoundingBox x1 y1 x2 y2 = b1
+          BoundingBox x1' y1' x2' y2' = b2
 
 instance Show BoundingBox where
     show (BoundingBox x1 y1 x2 y2) = "BoundingBox [" ++ points ++ "]"
